49382395e3e7e4f5a697fea581976e7e7bfa858a
Shiva Krishna Gopannagari <34725150+shivakrishna1995@users.noreply.github.com>
2023-01-12 22:34:06 +0530

    Dev: Added basic test cases

--- src/test/java/com/creditcardprocessing/api/CreditCardProcessingApplicationTests.java
+++ src/test/java/com/creditcardprocessing/api/CreditCardProcessingApplicationTests.java
@@ -1,13 +1,42 @@
 package com.creditcardprocessing.api;
 
+import com.creditcardprocessing.api.model.CreditCardModel;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import java.util.List;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CreditCardProcessingApplicationTests {
 
+	@Autowired
+	private TestRestTemplate testRestTemplate;
+
 	@Test
-	void contextLoads() {
+	public void addCreditCardTest() {
+		CreditCardModel cc = CreditCardModel.build(
+				0L,
+				"Test Name",
+				"122000000000003",
+				2000L,
+				0L
+		);
+		HttpEntity<CreditCardModel> request = new HttpEntity<>(cc);
+		ResponseEntity<CreditCardModel> response = testRestTemplate.postForEntity("/credit-card/add", request, CreditCardModel.class);
+		Assertions.assertEquals(HttpStatus.CREATED, response.getStatusCode());
+		Assertions.assertEquals("122000000000003", response.getBody().getCard_number());
 	}
 
+	@Test
+	public void getAllCreditCardsTest() {
+		ResponseEntity <CreditCardModel[]> response = testRestTemplate.getForEntity("/credit-card/all", CreditCardModel[].class);
+		Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+	}
 }
